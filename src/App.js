ee6dcfb4efe0d4df526e66b847889bac256666fd
shaharyar saleem <shaharyarsaleem71@gmail.com>
2022-05-23 16:56:38 +0200

    feat: userName component enabled

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import Repo from './Components/Repositories.jsx';
 import { BrowserRouter as Router, Switch, Route, Link, Routes} from "react-router-dom";
 import RepoDetail from "./Components/RepoDetail.jsx";
 import UserName from "./Components/UserName";
+import {useState} from 'react';
 
 function App() {
-  let github_name = "holoplot";
+
+  let [userName, setUserName] = useState('github')
+
+  let setGitUserName = (name) => {
+    console.log("UserName from APP.JS:", name)
+    setUserName(name)
+  }
+
   return (
     <div className="App">
+      <UserName setGitUserName={setGitUserName} />
       <Routes>
-        {/* <Route path="/" exact="true" element={<Repo name={github_name} />} /> */}
-        <Route path="/" exact="true" element={<UserName />} />
-        <Route path="/repos/holoplot/:name" element={<RepoDetail name={github_name} />} /> 
+        <Route path="/" exact="true" element={<Repo name={userName} key={userName} />} />
+        <Route path="/repos/:name/:name" element={<RepoDetail name={userName} key={userName} />} /> 
       </Routes>
     </div>
   );
--- src/Components/UserName.jsx
+++ src/Components/UserName.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default function UserName() {
+export default function UserName(props) {
     let inputForm = {
         justifyContent: 'center',
         textAlign: 'center',
         marginTop: '30px'
     }
+
+    let [userName, setUserName] = useState("")
+
+    let onChange = (e) => {
+        setUserName(e.target.value)
+    }
+
+    let onSubmitForm = () => {
+        props.setGitUserName(userName)
+    }
   return (
     <div class="input-group" style={inputForm}>
       <div class="form-outline">
-        <input type="search" class="form-control" placeholder="Enter User Name" />
+        <input type="search" class="form-control" placeholder="Enter User Name" onChange={onChange} />
       </div>
-      <button type="button" class="btn btn-primary">
+      <button type="button" class="btn btn-primary" onClick={onSubmitForm}>
         Search
       </button>
     </div>
